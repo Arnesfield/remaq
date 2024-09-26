572ace3946b7eaef797d850f692b7b34b923b7e4
Jefferson Rylee <rylee.jeff385@gmail.com>
2024-09-26 22:25:50 +0800

    Add eslint-plugin-chai-friendly and update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,10 +2,11 @@ module.exports = {
   root: true,
   env: { node: true },
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'chai-friendly'],
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  ignorePatterns: ['node_modules', 'lib', 'tmp', 'fixtures'],
+  ignorePatterns: ['node_modules', '/lib', 'tmp', 'fixtures'],
   rules: {
+    'chai-friendly/no-unused-expressions': 2,
     'no-constant-condition': 0,
     'no-unused-vars': 0,
     quotes: [1, 'single', { avoidEscape: true, allowTemplateLiterals: true }],
@@ -14,6 +15,7 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 0,
     '@typescript-eslint/no-inferrable-types': 0,
     '@typescript-eslint/no-non-null-assertion': 1,
+    '@typescript-eslint/no-unused-expressions': 0,
     '@typescript-eslint/no-unused-vars': [1, { argsIgnorePattern: '^_' }],
     '@typescript-eslint/no-var-requires': 0
   }
